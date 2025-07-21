84cdc6e2b69c85dc224faa206d887c68b839923d
devericlp <dev.ericlp@gmail.com>
2025-07-21 16:36:29 -0300

    refactor(release): changed release rules

--- release.config.js
+++ release.config.js
@@ -1,26 +1,32 @@
 export default {
-  branches: ['main'],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    [
-      '@semantic-release/release-notes-generator',
-      {
-        preset: 'conventionalcommits',
-      }
+    branches: ['main'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        [
+            '@semantic-release/release-notes-generator',
+            {
+                preset: 'conventionalcommits',
+                releaseRules: [
+                    {type: 'refactor', release: 'patch'},
+                ],
+                parserOpts: {
+                    noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+                },
+            }
+        ],
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: 'CHANGELOG.md',
+            }
+        ],
+        [
+            '@semantic-release/git',
+            {
+                assets: ['CHANGELOG.md'],
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            }
+        ],
+        '@semantic-release/github'
     ],
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      }
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      }
-    ],
-    '@semantic-release/github'
-  ],
 };
