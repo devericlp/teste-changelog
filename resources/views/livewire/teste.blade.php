e6b54958e3efdc2b5d750d0cbbc251668d6335ac
devericlp <dev.ericlp@gmail.com>
2025-07-21 16:38:22 -0300

    refactor(nada): change one thing

--- resources/views/livewire/teste.blade.php
+++ resources/views/livewire/teste.blade.php
@@ -1,6 +1,7 @@
 <div>
    @foreach($students as $student)
        <p>{{ $student['name'] }}</p>
+       <p>{{ $student['name'] }}</p>
    @endforeach
 
     <livewire:teste1 name="Eric" email="eric@email.com" lazy />
