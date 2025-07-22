0214475efb268c0546802549626283a509921ed9
devericlp <dev.ericlp@gmail.com>
2025-07-22 09:18:21 -0300

    feat: adiciona login
    Inclui middleware de autenticação com JWT.
    Protege rotas sensíveis com guard.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,6 +15,5 @@
             <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
         </div>
         <p>oi</p>
-        <p>oi 1</p>
     </div>
 </x-layouts.app>
